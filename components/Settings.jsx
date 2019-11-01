217bebaa726fd899c9fcb1aa9c6e0c1b8ccfe3dd
Soheab Malik <soheabdebeste@hotmail.com>
2019-11-01 23:41:54 +0100

    oops.

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -2,6 +2,7 @@ const {React} = require('powercord/webpack');
 const {Button} = require('powercord/components');
 const {TextInput} = require('powercord/components/settings');
 const { SwitchItem } = require('powercord/components/settings');
+const { shell } = window.require('electron');
 
 module.exports = class EightballSettings extends React.Component {
 		render() {
@@ -14,7 +15,7 @@ module.exports = class EightballSettings extends React.Component {
 								<div>
 								Enabled this to get the answers from the nekos.life api because why not.....
 								<br/>
-								See the list here without api: <a href={"#"} onClick={() => shell.openExternal("https://github.com/alexflipnote")}>Github</a>
+								See the list here without api: <a href={"#"} onClick={() => shell.openExternal("https://github.com/Soheab/eightball/blob/master/index.js#L23-L35")}>Github</a>
 								</div>
 							}
 		        >
